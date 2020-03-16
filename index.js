61e19109bd24cf25497fa95424b854dd2e279110
Jackson Chen <jacksonchen171@gmail.com>
2020-03-16 01:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   let ans = Object.assign({}, object, { [key] : value })
   return ans
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, { [key] : value })
+}
